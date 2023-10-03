2e70ef2d21a53a84e7d3ea0a7876d3efd3c104b7
Raj <4094.neeraj@gmail.com>
2023-10-03 23:13:48 +0530

    Second Task Done

--- task2.php
+++ task2.php
@@ -9,42 +9,42 @@
 
 <body>
 
-<?php
+    <?php
 
-$a = array(-1, -2, -10, -12, -15, 1, 2, 3, 4, 6, 12, 14); //Entered values
-arsort($a); //Array sort
-echo "Entered value are :- ";
-print_r($a);
-echo "<br>";
+    $a = array(-1, -2, -10, -12, -15, 1, 2, 3, 4, 6, 12, 14); //Entered values
+    arsort($a); //Array sort
+    echo "Entered value are :- ";
+    print_r($a);
+    echo "<br>";
 
-$n = current($a); //Used for knowing the last biggest +ve number
-$case = $a < 0 ? false : true; //Used to check the vaalues are +ve or -ve
-if (array_search($case, $a)) { //Run when the case is +ve
-    for ($i = 1; $i <= $n + 1; $i++) {
-        $a1[] = $i;
+    $n = current($a); //Used for knowing the last biggest +ve number
+    $case = $a < 0 ? false : true; //Used to check the vaalues are +ve or -ve
+    if (array_search($case, $a)) { //Run when the case is +ve
+        for ($i = 1; $i <= $n + 1; $i++) {
+            $a1[] = $i;
+        }
+    } else { //Run when the case is -ve
+        for ($i = 0; $i <= $n + 1; $i++) {
+            $a1[] = $i;
+        }
     }
-} else { //Run when the case is -ve
-    for ($i = 0; $i <= $n + 1; $i++) {
-        $a1[] = $i;
+
+    echo "<br>";
+
+    $a2 = $a1; //Store the values of for loop in the array
+    $result = array_diff($a2, $a); //Used to check the missing values
+    echo "Missing number lists from the aray :- ";
+    print_r($result);
+    $final = current($result); //Here I will go with the first value of the array
+    echo "<br>";
+    echo "<br>";
+    echo "First positive number that is missing from array is :- ";
+    if (current($result) == 0) { //If the first value is 0 then it will return 1
+        echo $final = 1;
+    } else { //If not shows my current first value
+        echo $final;
     }
-}
-
-echo "<br>";
-
-$a2 = $a1; //Store the values of for loop in the array
-$result = array_diff($a2, $a); //Used to check the missing values
-echo "Missing number lists from the aray :- ";
-print_r($result);
-$final = current($result); //Here I will go with the first value of the array
-echo "<br>";
-echo "<br>";
-echo "First positive number that is missing from array is :- ";
-if (current($result) == 0) { //If the first value is 0 then it will return 1
-    echo $final = 1;
-} else { //If not shows my current first value
-    echo $final;
-}
-?>
+    ?>
 </body>
 
 </html>
