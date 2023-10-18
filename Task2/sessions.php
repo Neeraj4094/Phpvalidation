a70239a48e32b8422c61dfe5a0d1dc5dafb63445
Raj <4094.neeraj@gmail.com>
2023-10-19 00:44:12 +0530

    Basic Changes in old files

--- Task2/sessions.php
+++ Task2/sessions.php
@@ -76,7 +76,7 @@ elseif (empty($password)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./dist/output.css">
+    <link rel="stylesheet" href="../dist/output.css">
     <title>Document</title>
 </head>
 
